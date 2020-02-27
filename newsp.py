166147a4e8f25015520e1204c992149e4b12490d
llliting <hllliting@gmail.com>
2020-02-26 21:00:47 -0500

    TFIDF

--- newsp.py
+++ newsp.py
@@ -8,13 +8,6 @@ from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords 
 #from wsjsample.py import sentimentScore
 
-fileName = "/Users/liting/Desktop/nlp/articles.txt"
-
-f = open(fileName,"r",encoding="utf-8" )
-
-urlList = f.readlines()
-articleList = []
-
 def removePunc(txt):
     remove = string.punctuation
     remove = remove.replace("-", "") # don't remove hyphens
@@ -23,32 +16,40 @@ def removePunc(txt):
     return txt
 
 
-for url in urlList:
-    try:
-        article = Article(url)
-        article.download()
-        article.parse()
-        #print(article.authors)
-        articleList += [article.text]
-    except Exception:
-        continue
-    
-
-dist = FreqDist()
-stop_words = set(stopwords.words('english')) 
-wnl = nltk.WordNetLemmatizer()
-ps = PorterStemmer()
 
 
-for t in articleList:      
-    t = removePunc(t)
-    for word in word_tokenize(t):
-        if not word in stop_words:
-            word = wnl.lemmatize(word)
-            word = ps.stem(word)
-            dist[word.lower()] += 1
+def main():
+    fileName = "/Users/liting/Desktop/nlp/articles.txt"
+    f = open(fileName,"r",encoding="utf-8" )
+    urlList = f.readlines()
+    articleList = []
+    
+    for url in urlList:
+        try:
+            article = Article(url)
+            article.download()
+            article.parse()
+            #print(article.authors)
+            articleList += [article.text]
+        except Exception:
+            continue
+        
+    
+    dist = FreqDist()
+    stop_words = set(stopwords.words('english')) 
+    wnl = nltk.WordNetLemmatizer()
+    ps = PorterStemmer()
     
     
+    for t in articleList:      
+        t = removePunc(t)
+        for word in word_tokenize(t):
+            if not word in stop_words:
+                word = wnl.lemmatize(word)
+                word = ps.stem(word)
+                dist[word.lower()] += 1
+main()
+        
+        
+       
     
-   
-
